5976d2d60bb393bee271bce58692dcc03be89b69
Shane Meyers <smeyers1031@gmail.com>
2022-08-06 11:59:40 -0700

    commit tree 02b97e918b4eefc58d5880f8585e82c48cb80c41

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet < 200) {
+    return "This one is on me!";
+  } else if (feet >= 400 && feet <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (feet > 2000 && feet <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  let response;
+  city === "NYC" ? (response = "Ok, sounds good.") : (response = "No go.");
+  return response;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  let complimentaryClosing;
+  switch (tip) {
+    case "generous":
+      complimentaryClosing = "Thank you so much.";
+      break;
+    case "not as generous":
+      complimentaryClosing = "Thank you.";
+      break;
+    default:
+      complimentaryClosing = "Bye.";
+      break;
+  }
+  return complimentaryClosing;
 }
